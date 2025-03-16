6962c4d7a3269e2921ee3e0662804844db171ae7
GiovanniSertorio <giovannistr@gmail.com>
2025-03-16 10:18:21 -0300

    teste 4 useasyncdata

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -361,7 +361,7 @@ const _inlineRuntimeConfig = {
         "headers": {
           "Content-Type": "text/xml; charset=UTF-8",
           "Cache-Control": "public, max-age=600, must-revalidate",
-          "X-Sitemap-Prerendered": "2025-03-16T13:09:12.541Z"
+          "X-Sitemap-Prerendered": "2025-03-16T13:18:09.075Z"
         }
       },
       "/_nuxt/builds/meta/**": {
