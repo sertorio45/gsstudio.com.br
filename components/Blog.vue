f717336d2e673eb2fd9256a0015b6cc2421b1f2a
GiovanniSertorio <giovannistr@gmail.com>
2025-03-16 09:31:49 -0300

    testando useAsyncData

--- components/Blog.vue
+++ components/Blog.vue
@@ -46,10 +46,11 @@
 </template>
 
 <script setup lang="ts">
-const { data: articles, pending, error } = useLazyFetch("https://painel.gsadmin.app/items/articles?fields=id,title,meta_keywords,meta_description,content,slug,categorie.id,categorie.title_categorie", {
-  transform: (response) => response.data.map(article => ({
+const { data: articles, pending, error } = await useAsyncData("articles", async () => {
+  const response = await $fetch("https://painel.gsadmin.app/items/articles?fields=id,title,meta_keywords,meta_description,content,slug,categorie.id,categorie.title_categorie");
+  return response.data.map(article => ({
     ...article,
     category_title: article.categorie?.title_categorie || "Sem categoria",
-  }))
+  }));
 });
 </script>
