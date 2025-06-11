989a69ad7c1292400ea8d5114b48b92caa667b5a
GiovanniSertorio <giovannistr@gmail.com>
2025-06-11 00:03:26 -0300

    netlify 19

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -368,7 +368,7 @@ const _inlineRuntimeConfig = {
         "headers": {
           "Content-Type": "text/xml; charset=UTF-8",
           "Cache-Control": "public, max-age=600, must-revalidate",
-          "X-Sitemap-Prerendered": "2025-06-11T03:02:03.079Z"
+          "X-Sitemap-Prerendered": "2025-06-11T03:02:44.946Z"
         }
       },
       "/_nuxt/builds/meta/**": {
