1cfd49bf6a93fe3f9285b99ff3bd2236f2188c85
GiovanniSertorio <giovannistr@gmail.com>
2025-06-11 23:14:49 -0300

    deploy 1

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -368,7 +368,7 @@ const _inlineRuntimeConfig = {
         "headers": {
           "Content-Type": "text/xml; charset=UTF-8",
           "Cache-Control": "public, max-age=600, must-revalidate",
-          "X-Sitemap-Prerendered": "2025-06-11T20:01:52.390Z"
+          "X-Sitemap-Prerendered": "2025-06-12T02:13:25.371Z"
         }
       },
       "/_nuxt/builds/meta/**": {
